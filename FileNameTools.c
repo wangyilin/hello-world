c4157a003d8e8dcda62fe8458b4c55745d341b55
ZhaoYan <zhaoyan.hrb@gmail.com>
2010-10-25 12:31:43 +0800

    add pointer check from ceeback

--- FileNameTools.c
+++ FileNameTools.c
@@ -7,6 +7,12 @@ int GetDir(char *whole,char* dir)
 	int len;                      /*储存长度*/
 	index=strrchr(whole,'/');     /*查找'/'在字符串whole中从后面开始的第一次出现的位置，并从此到末尾复制给index*/
 	len=index-whole;
+	if(NULL == dir) 
+	{ 
+		printf("Pointer is NULL @%s,%d",__FILE__,__LINE__); 
+		return -1;
+	} 
+
 	strncpy(dir,whole,len);       /*将字符串whole中前len个字符拷贝到字符串dir中*/
 	dir[len]='\0';                /*加上字符串结束符*/
 	return len;                   /*返回长度*/
@@ -20,6 +26,11 @@ int GetName(char *whole,char* Name)
     lname++;				//多复制了'/'，所以指针向右移一位
     rname=strrchr(whole,'.');		//拷贝拓展名
     len=rname-lname;			//得到不含拓展名的文件名的长度
+	if(NULL == Name) 
+	{ 
+		printf("Pointer is NULL @%s,%d",__FILE__,__LINE__); 
+		return -1;
+	}
     strncpy(Name,lname,len);		//得到不含拓展名的文件名
     Name[len]='\0';			//加上字符串结束符
     return len;				//返回文件名长度
@@ -33,6 +44,11 @@ int GetExt(char *whole,char* Ext)
    index=strrchr(whole,'.');		//将拓展名(也就是文件名后缀)拷贝
    index++;				//多复制了'.'，故指针右移一位
    len=strlen(whole)-strlen(index);	//得到拓展名的长度
+   if(NULL == Ext) 
+	{ 
+		printf("Pointer is NULL @%s,%d",__FILE__,__LINE__); 
+		return -1;
+	}
    strncpy(Ext,index,len);		//拷贝拓展名
    Ext[len]='\0';			//加上字符串结束符
    return len;				//返回拓展名长度
@@ -41,7 +57,8 @@ int GetExt(char *whole,char* Ext)
 void TestFileNameTools(void)
 {
 	char whole[]="c:/windows/system32/abc.dll";
-	char des[256];
+	/*char des[256];*/
+	char *des = NULL;
 
 	GetDir(whole,des);
 	printf("%s\n",des);
